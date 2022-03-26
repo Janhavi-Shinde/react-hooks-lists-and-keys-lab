92105ccded9704aea9904aafff8633cb5e79ab58
janhavi <janashinde394@gmail.com>
2022-03-26 22:05:34 +1100

    all tests passing - navBar to be improved

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = ["#home", "#about", "#projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navbarElements = links.map((link) => { 
+    return (<a key={link} href={link}> {link} </a>)
+  });
+
+  return <nav>{navbarElements}</nav>;
 }
 
 export default NavBar;
